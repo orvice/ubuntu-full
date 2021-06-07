ce64d60478e3926087ab6191a840d0242970b20a
orvice <orvice@gmail.com>
2021-06-07 12:41:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y unzip curl
+
+echo "Update"
+apt-get update 
+
+echo "Install unzip curl wget "
+apt-get install -y unzip curl wget
+
+echo "Install Mysql Client"
+apt-get install -y default-mysql-client
+
+echo "Intall rclone"
 curl https://rclone.org/install.sh | bash
