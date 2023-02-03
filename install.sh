cc3f5a901666a0a28dcd3121a999d2bac27f00b3
orvice L <orvice@gmail.com>
2023-02-03 11:49:23 +0800

    add azure cli

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Update"
 apt-get update 
 
 echo "Install curl wget gnupg2 ca-certificates"
-apt-get install -y git unzip curl wget gnupg2 ca-certificates
+apt-get install -y git unzip curl wget gnupg2 ca-certificates sudo
 
 echo "Install Mysql Client"
 apt-get install -y default-mysql-client
@@ -21,3 +21,23 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc |  apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" |  tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 apt-get update && apt-get install -y mongodb-org-shell
+
+
+# azure cli
+echo "Install azure cli"
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt-get update
+sudo apt-get install azure-cli
