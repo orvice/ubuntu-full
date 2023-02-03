47ff5aa6097f2c5ab157ed7c8f52fd23544d95e7
orvice L <orvice@gmail.com>
2023-02-03 11:49:55 +0800

    add terraform

--- install.sh
+++ install.sh
@@ -41,3 +41,9 @@ echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsof
 
 sudo apt-get update
 sudo apt-get install azure-cli
+
+# terraform
+echo "Install terraform"
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
